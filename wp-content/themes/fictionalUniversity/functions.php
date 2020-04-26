2926fe7b7bdfd82993b943da3c62dd87e61546e4
nabil daoud <nabildaoud45@gmail.com>
2020-04-26 03:05:20 +0100

    Wordpress Interior Page Template

--- wp-content/themes/fictionalUniversity/functions.php
+++ wp-content/themes/fictionalUniversity/functions.php
@@ -6,5 +6,11 @@
 		wp_enqueue_style('font-google', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 	}
 	add_action('wp_enqueue_scripts', 'style_file');
+
+	function university_features(){
+		add_theme_support('title-tag');
+	}
+
+	add_action('after_setup_theme','university_features')
 ?>
 
